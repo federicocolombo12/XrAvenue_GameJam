a6ecea2f5d67424747b2b46b7eb4b3a57c963ce8
federicocolombo12 <chiccoclub5@gmail.com>
2026-05-31 16:21:28 +0200

    Baby Waste

--- Assets/_Project/Scripts/Core/WasteType.cs
+++ Assets/_Project/Scripts/Core/WasteType.cs
@@ -7,8 +7,11 @@ namespace AvenueXR.Core
         Glass,
         Metal,
         Organic,
-        Moral, // Oggetti speciali (es. Sacca di sangue, Bimbo)
+        Moral, // Oggetti speciali (es. Sacca di sangue)
         Gore,  // Pezzi umani
-        Bomb   // Oggetto della cospirazione
+        Bomb,  // Oggetto della cospirazione
+        Card,  // Nuova categoria
+        Baby,  // Nuova categoria
+        Box    // Nuova categoria
     }
 }
